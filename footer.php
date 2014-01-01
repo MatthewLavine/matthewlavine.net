a17b7e45fa1fa10876478c01fadbba74cb0f719a
Matthew Lavine <hermitlavine@gmail.com>
2013-12-31 23:14:41 -0500

    Switched to local jQuery version.

--- footer.php
+++ footer.php
@@ -5,8 +5,7 @@
 	        </div>
         </div>
 
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+	    <script src="js/vendor/jquery-1.10.2.min.js"></script>
         <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
         <script src="https://code.jquery.com/jquery.js"></script>
