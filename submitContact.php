5ee9de7607fe86dfbe74f8349fa8595355a6736d
Matthew Lavine <hermitlavine@gmail.com>
2014-05-16 16:35:54 -0400

    Edited smtp headers

--- submitContact.php
+++ submitContact.php
@@ -10,7 +10,7 @@ if($_POST){
 
 	$headers  = 'MIME-Version: 1.0' . "\r\n";
 	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-	$headers .= 'From: NJIT Website <msl23_web@njit.edu>' . "\r\n";
+	$headers .= 'From: Portfolio Website <skynet@matthewlavine.net>' . "\r\n";
 
 	mail('msl23@njit.edu', $subject, $body, $headers);
 }
