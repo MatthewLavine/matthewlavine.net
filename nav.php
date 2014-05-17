4190802a86bbbd32d8bc4e3ab18517b7c28e936f
Matthew Lavine <hermitlavine@gmail.com>
2014-05-17 12:29:29 -0400

    Consolidated project links

--- nav.php
+++ nav.php
@@ -1,50 +1,45 @@
 <div class="navbar navbar-default navbar-fixed-top" role="navigation">
-<div class="container">
+  <div class="container">
 
-<div class="navbar-header">
+    <div class="navbar-header">
     	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#nav1">
-      		<span class="sr-only">Toggle navigation</span>
-      		<span class="icon-bar"></span>
-     	 	<span class="icon-bar"></span>
-      		<span class="icon-bar"></span>
-    	</button>
-	<a class="navbar-brand" href="#">Matthew Lavine</a>
-</div>
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Matthew Lavine</a>
+    </div>
 
-<div class="collapse navbar-collapse" id="nav1">
-	<ul class="nav navbar-nav">
-      		<li class="active"><a data-toggle="tab" href="#aboutMe">About Me</a></li>
-
-      		<li class="dropdown">
-        		<a href="#" class="dropdown-toggle" data-toggle="dropdown">Projects<b class="caret"></b></a>
-			<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu2">
-				<li role="presentation" class="dropdown-header">Class Projects</li>
-          			<li><a href="#" data-toggle="modal" data-target="#it114">IT114</a></li>
-          			<li class="disabled"><a href="#">IT201</a></li>
-          			<li class="disabled"><a href="#">IT202</a></li>
-          			<li><a target="_blank" href="http://wickr-dev.voltagenj.com/">IT310 (Wickr)</a></li>
-				<li role="presentation" class="divider"></li>
-				<li role="presentation" class="dropdown-header">Personal Projects</li>
-          			<li><a href="http://web.njit.edu/~msl23/chris/" target="_blank">chris.harrsch.net</a></li>
-                <li><a href="http://chat.hardorange.org/" target="_blank">chat.hardorange.org</a></li>
-          			<li><a href="http://web.njit.edu/~msl23/bootstrap/bootstrap.zip">Bootstrap Deploy Package</a></li>
-                <li><a href="http://krhs.net/" target="_blank">Kittatinny Regional High School</a></li>
-
-
-			</ul>
-		</li>
-          <li><a target ="_blank" href="https://www.dropbox.com/s/lcma0we484n79dv/Resume.pdf">Resume</a></li>
-      		<li><a data-toggle="tab" href="#contact">Contact Me</a></li>		
-    	</ul>
-
-    	<ul class="nav navbar-nav navbar-right">
-          	<!--<li><a href="#">Facebook</a></li>-->
-        	<!--<li><a href="#">Twitter</a></li>-->
-		      <!--<li><a target="_blank" href="http://advancedgroup.net/">AdvancedGroup</a></li>-->
-		      <li><a target="_blank" href="https://www.linkedin.com/in/lavine"><i class="fa fa-linkedin-square"></i> LinkedIn</a></li>
-         	<li><a target="_blank" href="https://github.com/MatthewLavine"><i class="fa fa-github"></i> GitHub</a></li>
-	 </ul>
-</div><!-- /.navbar-collapse -->
+    <div class="collapse navbar-collapse" id="nav1">
+     <ul class="nav navbar-nav">
+      <li class="active"><a data-toggle="tab" href="#aboutMe">About Me</a></li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Projects<b class="caret"></b></a>
+        <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu2">
+          <li role="presentation" class="dropdown-header">Class Projects</li>
+          <li><a href="#" data-toggle="modal" data-target="#it114">IT114</a></li>
+          <li class="disabled"><a href="#">IT201</a></li>
+          <li class="disabled"><a href="#">IT202</a></li>
+          <li><a target="_blank" href="http://wickr-dev.voltagenj.com/">IT310 (Wickr)</a></li>
+          <li role="presentation" class="divider"></li>
+          <li role="presentation" class="dropdown-header">Personal Projects</li>
+          <li><a href="http://chat.hardorange.org/" target="_blank">chat.hardorange.org</a></li>
+          <li><a href="http://krhs.net/" target="_blank">Kittatinny Regional High School</a></li>
+          <li><a href="http://mckeown.org/" target="_blank">Marian E. McKeown School</a></li>
+        </ul>
+      </li>
+      <li><a target ="_blank" href="https://www.dropbox.com/s/lcma0we484n79dv/Resume.pdf">Resume</a></li>
+      <li><a data-toggle="tab" href="#contact">Contact Me</a></li>		
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+     <!--<li><a href="#">Facebook</a></li>-->
+     <!--<li><a href="#">Twitter</a></li>-->
+     <!--<li><a target="_blank" href="http://advancedgroup.net/">AdvancedGroup</a></li>-->
+     <li><a target="_blank" href="https://www.linkedin.com/in/lavine"><i class="fa fa-linkedin-square"></i> LinkedIn</a></li>
+     <li><a target="_blank" href="https://github.com/MatthewLavine"><i class="fa fa-github"></i> GitHub</a></li>
+   </ul>
+ </div><!-- /.navbar-collapse -->
 
 </div>
 </div>
@@ -56,10 +51,10 @@
 <!-- Modal Windows -->
 <script>
 $('.modal').on('show.bs.modal', function () {
-        $('.modal').animate({ scrollTop: 0 }, 'slow');
+  $('.modal').animate({ scrollTop: 0 }, 'slow');
 });
 $('.modal').on('hide.bs.modal', function () {
-        $('.modal').animate({ scrollTop: 0 }, 'slow');
+  $('.modal').animate({ scrollTop: 0 }, 'slow');
 });
 </script>
 
