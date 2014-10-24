e72fa40ebedc4211f05ae1f97ec02f3c2b3b4e47
Matthew Lavine <hermitlavine@gmail.com>
2014-10-24 14:21:15 -0400

    Updated favicon locations

--- header.php
+++ header.php
@@ -10,8 +10,8 @@
 		<meta name="description" content="Matthew Lavine's Personal Portfolio">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		
-		<link rel="shortcut icon" href="<?php echo RELATIVE_WEB_ROOT; ?>favicon.ico" type="image/x-icon">
-		<link rel="icon" href="<?php echo RELATIVE_WEB_ROOT; ?>favicon.ico" type="image/x-icon">
+		<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+		<link rel="icon" href="/favicon.ico" type="image/x-icon">
 
 		<link rel="stylesheet" href="css/normalize.css">
 		<link rel="stylesheet" href="css/main.css">
--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<?php require_once 'application.php' ?>
 <?php include 'header.php' ?>
 </head>
 
