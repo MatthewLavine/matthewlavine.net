aa0fbb51b92bf9fb0b7ef7ec0d048ee1cac45445
Matthew Lavine <hermitlavine@gmail.com>
2014-12-15 12:34:35 -0500

    Added mobile compatibility

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
-$(function() {    
-    function doScroll() {
-        $(".arrow").animate({"opacity" : "0"}, 300, function() {
-            $(".arrow").css("display", "none");
-        })
-        $(".info").animate({"padding-top":"3em"}, 600, function() {
-            $("section, footer").css("display", "inline-block").animate({"opacity" : "1"}, 400);
-        });
-        $(window).unbind('mousewheel DOMMouseScroll', handler);
-    }
-    
-    var handler = function(event){
-        if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
+$(function() {
+    var mq = window.matchMedia('@media (max-width: 906px)');
+    if(mq.matches) {
+        // disable interactivity
+    } else {
+        function doScroll() {
+            $(".arrow").animate({"opacity" : "0"}, 300, function() {
+                $(".arrow").css("display", "none");
+            })
+            $(".info").animate({"padding-top":"3em"}, 600, function() {
+                $("section, footer").css("display", "inline-block").animate({"opacity" : "1"}, 400);
+            });
+            $(window).unbind('mousewheel DOMMouseScroll', handler);
+        }
+
+        var handler = function(event){
+            if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
             // mouse scrolled up
         } else {
             doScroll();
@@ -18,4 +22,5 @@ $(function() {
     }
     $(window).bind('mousewheel DOMMouseScroll', handler);
     $(".arrow").click(doScroll);
+    }
 });
