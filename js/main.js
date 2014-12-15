3fc414b034a4a4c52a09b4b1d83408ef3fc1f61b
Matthew Lavine <hermitlavine@gmail.com>
2014-12-15 12:26:10 -0500

    Added initial banner with dynamic reveal

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function() {    
+    function doScroll() {
+        $(".arrow").animate({"opacity" : "0"}, 300, function() {
+            $(".arrow").css("display", "none");
+        })
+        $(".info").animate({"padding-top":"3em"}, 600, function() {
+            $("section, footer").css("display", "inline-block").animate({"opacity" : "1"}, 400);
+        });
+        $(window).unbind('mousewheel DOMMouseScroll', handler);
+    }
+    
+    var handler = function(event){
+        if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
+            // mouse scrolled up
+        } else {
+            doScroll();
+        }
+    }
+    $(window).bind('mousewheel DOMMouseScroll', handler);
+    $(".arrow").click(doScroll);
+});
