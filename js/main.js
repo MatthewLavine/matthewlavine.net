405fc7cd5d09b9ae18e3dba593f64c84102a373a
Matthew Lavine <hermitlavine@gmail.com>
2014-12-16 16:17:08 -0500

    Technology Cloud

--- js/main.js
+++ js/main.js
@@ -15,11 +15,50 @@ $(function() {
         var handler = function(event){
             if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
             // mouse scrolled up
-        } else {
-            doScroll();
+            } else {
+                doScroll();
+            }
         }
-    }
-    $(window).bind('mousewheel DOMMouseScroll', handler);
-    $(".arrow").click(doScroll);
+        $(window).bind('mousewheel DOMMouseScroll', handler);
+        $(".arrow").click(doScroll);
+
+        function shuffleArray(array) {
+            for (var i = array.length - 1; i > 0; i--) {
+                var j = Math.floor(Math.random() * (i + 1));
+                var temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+            return array;
+        }
+        var techs = [];
+        $('.project > a').each(function() {
+            $.each($(this).data('technologies').split(', '), function() {
+                var el = "<span>" + this  + "</span>";
+                if((techs.indexOf("<span>" + this  + "</span>")) < 0) {
+                    techs.push(el);
+                }
+            });
+        });
+        techs = shuffleArray(techs);
+        $.each(techs, function() {
+            $('.skill-container').append(this);
+        })
+        $('.project > a').hover(function() {
+            var proj = $(this);
+            $.each(proj.data('technologies').split(', '), function() {
+                var p = this.toString();
+                $(".skill-container").children().each(function() {
+                    var k = $(this).contents().text();
+                    if(p == k) {
+                        console.log($(this));
+                        $(this).addClass('highlight');
+                    }
+                });
+            });
+            
+        }, function() {
+            $(".skill-container").children().removeClass('highlight');
+        });
     }
 });
